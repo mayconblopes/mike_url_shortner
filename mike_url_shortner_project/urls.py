33d3551320c36a00dd73e9501ac7f2e00ea3677b
Lopes, Maycon <lopes.maycon@gmail.com>
2022-05-14 00:05:37 -0300

    aaaaaa

--- mike_url_shortner_project/urls.py
+++ mike_url_shortner_project/urls.py
@@ -1,4 +1,4 @@
-"""mike_url_shotner_project URL Configuration
+"""mike_url_shortner_project URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
--- mike_url_shortner_project/wsgi.py
+++ mike_url_shortner_project/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for mike_url_shotner_project project.
+WSGI config for mike_url_shortner_project project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mike_url_shotner_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mike_url_shortner_project.settings')
 
 application = get_wsgi_application()
